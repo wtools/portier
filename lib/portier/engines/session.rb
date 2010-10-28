cb5df4cd3de26124bd7a702d25f8f9e524837ea7
Roland Venesz <roland.venesz@wopata.com>
2010-10-28 21:23:01 +0200

    Module encapsulation fix

--- lib/portier/engines/session.rb
+++ lib/portier/engines/session.rb
@@ -1,24 +1,26 @@
-module Portier::Engines
-  module Session
-    protected
+module Portier
+  module Engines
+    module Session
+      protected
 
-    def authenticate_from_session
-      if session[:current_user_id]
-        begin
-          self.class.user_model_for(:session).find session[:current_user_id]
-        rescue
-          remove_user_from_session
+      def authenticate_from_session
+        if session[:current_user_id]
+          begin
+            self.class.user_model_for(:session).find session[:current_user_id]
+          rescue
+            remove_user_from_session
+          end
         end
       end
-    end
 
-    def persist_user_into_session user
-      @current_user = user
-      session[:current_user_id] = user.send(user.class.primary_key)
-    end
+      def persist_user_into_session user
+        @current_user = user
+        session[:current_user_id] = user.send(user.class.primary_key)
+      end
 
-    def remove_user_from_session
-      session.delete :current_user_id
+      def remove_user_from_session
+        session.delete :current_user_id
+      end
     end
   end
 end
